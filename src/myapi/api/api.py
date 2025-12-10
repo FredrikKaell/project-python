854c76eb892a67c223031a7434612b8cd5b59499
FredrikKaell <fredrik.kall@outlook.com>
2025-12-10 13:47:49 +0100

    Added application entry point and fastapi base

---
+++ src/myapi/api/api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware. cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/")
+def root():
+    """_summary_
+
+    Returns:
+        _type_: str
+    """
+    return "This is the root"
